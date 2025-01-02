39e483c3a056aa32ca30b03643a30287829973c8
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2025-01-02 21:47:51 +0530

    Time: 4 ms (98.18%), Space: 85.8 MB (48.29%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,39 +1,34 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] arr) {
-        int size=words.length;
-        boolean isFound[]=new boolean[size];
-        int count=0;
-        for(int i=0;i<size;i++){
-            isFound[i]=containsVowels(words[i]);
+        int size = words.length;
+        boolean isFound[] = new boolean[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            isFound[i] = containsVowels(words[i]);
         }
-        int currCount[]=new int[size];
-        for(int i=0;i<size;i++)
-        {
-            if(isFound[i])
-            {
+        int currCount[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            if (isFound[i]) {
                 count++;
             }
-            currCount[i]=count;
+            currCount[i] = count;
         }
-        int n=arr.length;
-        int ans[]=new int[n];
-        for(int i=0;i<n;i++)
-        {
-            if(arr[i][1]==arr[i][0])
-            {
-                if(isFound[arr[i][1]])
-                ans[i]=1;
+        int n = arr.length;
+        int ans[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            if (arr[i][1] == arr[i][0]) {
+                if (isFound[arr[i][1]])
+                    ans[i] = 1;
                 else
-                ans[i]=0;
-            }else if(isFound[arr[i][0] ])
-            {
-                ans[i]=currCount[arr[i][1]]-currCount[arr[i][0]]+1;
-            }else
-                ans[i]=currCount[arr[i][1]]-currCount[arr[i][0]];
+                    ans[i] = 0;
+            } else if (isFound[arr[i][0]]) {
+                ans[i] = currCount[arr[i][1]] - currCount[arr[i][0]] + 1;
+            } else
+                ans[i] = currCount[arr[i][1]] - currCount[arr[i][0]];
 
-        } 
-        return ans;      
-        
+        }
+        return ans;
+
     }
 
     private static boolean containsVowels(String s) {
