ad7015784ffa21cc10dc1f349968e2b371f84d4b
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2025-01-06 17:20:42 +0530

    Time: 98 ms (40.1%), Space: 45.2 MB (14.93%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,29 +1,28 @@
 class Solution {
     public int[] minOperations(String boxes) {
-        int len=boxes.length();
-        int pre[]=new int[len];
-        int post[]=new int[len];
-        int answer[]=new int[len];
-        int preCount=0;
-        int postCount=0;
-        if(boxes.charAt(0)=='1')
-        preCount=1;
-        if(boxes.charAt(len-1)=='1')
-        postCount=1;
-        for(int i=1;i<len;i++)
-        {
-            pre[i]=preCount+pre[i-1];
-            post[len-i-1]=postCount+post[len-i];
-            if(boxes.charAt(i)=='1')
-            preCount++;
-            if(boxes.charAt(len-i-1)=='1')
-            postCount++;
+        int len = boxes.length();
+        int pre[] = new int[len];
+        int post[] = new int[len];
+        int answer[] = new int[len];
+        int preCount = 0;
+        int postCount = 0;
+        if (boxes.charAt(0) == '1')
+            preCount = 1;
+        if (boxes.charAt(len - 1) == '1')
+            postCount = 1;
+        for (int i = 1; i < len; i++) {
+            pre[i] = preCount + pre[i - 1];
+            post[len - i - 1] = postCount + post[len - i];
+            if (boxes.charAt(i) == '1')
+                preCount++;
+            if (boxes.charAt(len - i - 1) == '1')
+                postCount++;
         }
-        for(int i=0;i<len;i++){
-            answer[i]=pre[i]+post[i];
-            System.out.println(pre[i]+" "+ post[i]);
+        for (int i = 0; i < len; i++) {
+            answer[i] = pre[i] + post[i];
+            System.out.println(pre[i] + " " + post[i]);
         }
-        
+
         return answer;
     }
 }
