d9f44c88bd2626082c8cb365e5f68cf1fae6d790
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2025-01-10 10:38:08 +0530

    Time: 12 ms (92.44%), Space: 54.2 MB (86.05%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.java
@@ -0,0 +1,33 @@
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        int count[] = new int[26];
+        List<String> universal = new ArrayList<>();
+        int m = words2.length;
+        int n = words1.length;
+        for (int i = 0; i < m; i++) {
+            int [] maxCount=new int[26];
+            int len = words2[i].length();
+            String str = words2[i];
+            for (int j = 0; j < len; j++)
+            {
+                maxCount[str.charAt(j) - 'a']++;
+                count[str.charAt(j) - 'a']=Math.max(maxCount[str.charAt(j) - 'a'],count[str.charAt(j) - 'a']);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            int occ[] = new int[26];
+            int len = words1[i].length();
+            String str = words1[i];
+            for (int j = 0; j < len; j++)
+                occ[str.charAt(j) - 'a']++;
+            boolean flag = true;
+            for (int j = 0; j < 26; j++)
+                if (count[j] > occ[j])
+                    flag = false;
+            if (flag)
+                universal.add(words1[i]);
+        }
+
+        return universal;
+    }
+}
