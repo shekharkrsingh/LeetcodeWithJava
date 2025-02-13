bf37480baff0fc748a8eb88d40d45479bd6681ad
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2025-02-13 10:29:19 +0530

    Time: 111 ms (14.21%), Space: 63.1 MB (22.2%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.java
@@ -1,23 +1,30 @@
-import java.util.*;
-
 class Solution {
     public int maximumSum(int[] nums) {
-        Map<Integer, int[]> map = new HashMap<>();
+        int n = nums.length;
         int maxVal = -1;
-        for (int num : nums) {
-            int sum = sumOfDegi(num);
-            int[] largest = map.getOrDefault(sum, new int[] { -1, -1 });
-            if (num > largest[0]) {
-                largest[1] = largest[0];
-                largest[0] = num;
-            } else if (num > largest[1])
-                largest[1] = num;
-            map.put(sum, largest);
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int sum = sumOfDegi(nums[i]);
+        
+            if (map.containsKey(sum)) {
+                List<Integer> list = map.get(sum);
+                list.add(nums[i]);
+                map.put(sum, list);
+            } else {
+                List<Integer> list = new ArrayList<>();
+                list.add(nums[i]);
+                map.put(sum, list);
+            }
         }
-        for (int[] largest : map.values())
-            if (largest[1] != -1)
-                maxVal = Math.max(maxVal, largest[0] + largest[1]);
+        for (Map.Entry<Integer, List<Integer>> a : map.entrySet()) {
+            List<Integer> list = a.getValue();
+            int size = list.size();
+            Collections.sort(list);
+            if (size > 1)
+                maxVal = Math.max(maxVal, list.get(size - 2) + list.get(size - 1));
+        }
         return maxVal;
+
     }
 
     private int sumOfDegi(int num) {
