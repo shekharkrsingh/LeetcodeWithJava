cb9d6c38259dd84825ad2dd4df81d171eb710bdc
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2025-03-10 21:56:51 +0530

    Time: 1 ms (73.53%), Space: 40.8 MB (89.26%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,21 +1,15 @@
+import java.util.Arrays;
+
 class Solution {
     public int combinationSum4(int[] nums, int target) {
+        int n = nums.length;
         int[] dp = new int[target + 1];
-        Arrays.fill(dp, -1);
-        return solution(nums, target, dp);
-    }
+        dp[0] = 1;
 
-    private int solution(int[] nums, int target, int[] dp) {
-        if (target == 0)
-            return 1;
-        if (target < 0)
-            return 0;
-        if (dp[target] != -1)
-            return dp[target];
-        int count = 0;
-        for (int a : nums)
-            count += solution(nums, target - a, dp);
-        dp[target] = count;
-        return count;
+        for (int i = 1; i <= target; i++)
+            for (int j = 0; j < n; j++)
+                if (i - nums[j] >= 0)
+                    dp[i] += dp[i - nums[j]];
+        return dp[target];
     }
 }
