31eff8b3027747819d14cc6bf5964e2fdde5430a
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2025-04-20 20:49:27 +0530

    Time: 4 ms (30.84%), Space: 42.4 MB (41.97%) - LeetHub

--- 0797-rabbits-in-forest/0797-rabbits-in-forest.java
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.java
@@ -2,15 +2,14 @@ class Solution {
     public int numRabbits(int[] answers) {
         Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
-        for (int it : answers) {
-            if (it == 0) {
+        for (int it : answers)
+            if (it == 0)
                 count++;
-            } else {
+            else {
                 if (!map.containsKey(it))
                     map.put(it, 0);
                 map.put(it, map.get(it) + 1);
             }
-        }
         for (Map.Entry<Integer, Integer> it : map.entrySet()) {
             int key = it.getKey() + 1;
             int val = it.getValue();
