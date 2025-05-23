3442e5c754fd7f259c01721c735128f5c9446334
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2025-05-23 23:13:39 +0530

    Time: 21 ms (54.12%), Space: 56.6 MB (14.12%) - LeetHub

---
+++ 3264-maximum-points-after-enemy-battles/3264-maximum-points-after-enemy-battles.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long maximumPoints(int[] enemyEnergies, int currentEnergy) {
+        Arrays.sort(enemyEnergies);
+        int n = enemyEnergies.length;
+        if (currentEnergy < enemyEnergies[0])
+            return 0;
+        long sum = currentEnergy;
+        for (int i = 1; i < n; i++)
+            sum += enemyEnergies[i];
+        return sum / enemyEnergies[0];
+    }
+}
