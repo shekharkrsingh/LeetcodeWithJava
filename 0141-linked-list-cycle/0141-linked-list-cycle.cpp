3c0a70d77769e4d5afd5c66d7e3bd451824f4c3f
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2025-06-15 23:11:18 +0530

    Time: 12 ms (40.11%), Space: 12 MB (23.35%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -11,13 +11,14 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode* first=head;
         ListNode* second=head;
-        while(true){
-            if(first==NULL || second==NULL || first->next==NULL)
+        while(first!=NULL){
+            if(first->next==NULL)
             return false;
             first=first->next->next;
             second=second->next;
             if(first==second)
             return true;
         }
+        return false;
     }
 };
