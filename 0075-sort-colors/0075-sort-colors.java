fe6a616b3f3bb3b27f8e91f90601269beaeb02b1
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2025-09-02 12:14:08 +0530

    Time: 0 ms (100%), Space: 42.1 MB (47.38%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,38 +1,37 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int one=0;
-        int two=0;
-        int zero=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==1)
-            one++;
-            if(nums[i]==0)
-            zero++;
-            if(nums[i]==2)
-            two++;
-        }System.out.println(zero);
-        System.out.println(one);
-        System.out.println(two);
-        for(int i=0;i<nums.length;i++)
-        {
-            if(zero!=0)
-            {
-                nums[i]=0;
-                zero--;
-                continue;
+        int n=nums.length;
+        int first=0;
+        int second=0;
+        while(second<n){
+            if(nums[second]==0){
+                swap(first, second, nums);
+                first++;
+            }else{
+                second++;
             }
-            if(zero==0 && one!=0)
-            {
-                nums[i]=1;
-                one--;
-                continue;
-            }
-            if(one==0 && zero==0)
-            {
-                nums[i]=2;
-                continue;
+            if(first>second){
+                second=first;
             }
         }
+        first=n-1;
+        second=n-1;
+        while(second>=0){
+            if(nums[second]==2){
+                swap(first, second, nums);
+                first--;
+            }else{
+                second--;
+            }
+            if(first<second){
+                second=first;
+            }
+        }
+    }
+
+    private void swap(int i, int j, int[] nums){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
