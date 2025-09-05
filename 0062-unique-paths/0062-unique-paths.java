82d1d01b0e2c5a9f9fa9b4a79646843c537e3844
Shekhar Singh <107613589+shekharkrsingh@users.noreply.github.com>
2025-09-05 14:59:42 +0530

    Time: 0 ms (100%), Space: 41.2 MB (5.77%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,47 +1,25 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++)
-            {
-                dp[i][j]=-1;
+    public int uniquePaths(int n, int m) {
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = -1;
             }
         }
-        return sol(m, n, dp);
+        return path(0, 0, n, m, dp);
     }
 
-    private int sol(int m ,int n, int[][] dp)
-    {
-        if(m==1 && n==1)
-        return 1;
-        if(m==0 || n==0)
-        return 0;
-        if(dp[m-1][n-1]!=-1)
-        return dp[m-1][n-1];
-        int right= sol(m, n-1, dp);
-        int down= sol(m-1, n, dp);
-        dp[m-1][n-1]=right+down;
-        return right+down;
+    private int path(int i, int j, int n, int m, int[][] dp) {
+        if (i >= n || j >= m)
+            return 0;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        if (i == n - 1 && j == m - 1)
+            return 1;
+        int total = 0;
+        total += path(i + 1, j, n, m, dp);
+        total += path(i, j + 1, n, m, dp);
+        dp[i][j] = total;
+        return total;
     }
-
-
 }
-
-
-
-
-
-
-
-// class Solution {
-//     public int uniquePaths(int m, int n) {
-//         if(m==1 && n==1)
-//         return 1;
-//         if(m==0 || n==0)
-//         return 0;
-//         int right= uniquePaths(m, n-1);
-//         int down= uniquePaths(m-1, n);
-//         return right+down;
-//     }
-
-// }
